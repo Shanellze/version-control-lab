7ff9dfdf6c7db6cc2fd7085177c6351dffb1592b
Shanell Pereira <shanell.pereira@yahoo.co.uk>
2023-10-21 19:25:35 +0100

    initial commit.

---
+++ stats.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int x = 0;
+  int y = 0;
+  
+  cout << "Please enter the first number:";
+  cin >> x;
+  cout << "Please enter the second number:";
+  cin >> y;
+  cout << "The sum of " << x << " and " << y
+  << " is: "<< x + y << std::endl;
+  
+  cout << "The difference of " << x << " and " << y
+  << " is: "<< y - x << std::endl;
+  
+  cout << "The product of " << x << " and " << y
+  << " is: "<< x * y << std::endl;
+  
+  cout << "The distance of " << x << " and " << y
+  << " is: "<< abs(y-x) << std::endl;
+  
+  cout << "The mean average of " << x << " and " << y
+  << " is: "<< (x + y)/2 << std::endl;
+  
+  return 0;
+}
